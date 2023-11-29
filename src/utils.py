de4ca74511021175fef23cee2c8b7c63c144d107
mlpotter <mlpotter96@gmail.com>
2023-11-29 18:50:46 -0500

    fix the label encoding for y

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 from sklearn.datasets import load_breast_cancer,fetch_20newsgroups_vectorized,fetch_openml
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
 import pandas as pd
 def load_dataset(dataset):
     X, y = fetch_openml(dataset,return_X_y=True,as_frame=True)
@@ -9,7 +11,9 @@ def load_dataset(dataset):
 
     X = pd.get_dummies(X,columns=X.columns[X.dtypes == "object"])
 
-    X = X.values.astype(float); y=y.values.astype(float)
+    label_encoder = LabelEncoder()
+
+    X = X.values.astype(float); y=label_encoder.fit_transform(y).astype(float)
     print("Dataset Shape: ",X.shape)
 
     X_tr,X_te,y_tr,y_te = train_test_split(X,y,test_size=0.2,stratify=y,random_state=123)
