bddd0d83248f21871e994cfd1ec5b688deba3275
nishanthmarer <125854808+nishanthmarer@users.noreply.github.com>
2023-12-09 12:25:01 -0500

    Add files via upload

---
+++ discovery_submit.py
@@ -0,0 +1,63 @@
+import os
+import re
+
+import numpy as np
+from subprocess import Popen
+import pdb
+
+import time
+
+os.makedirs(os.path.join("logs"),exist_ok=True)
+
+blocking=False
+
+
+
+models = ["logistic","mlp","xgboost"]
+datasets = ["gina_agnostic","hiva_agnostic","sylva_agnostic"]
+algorithms = ["ga_seq","ga_joblib","random","rfs"]
+metrics = ["f1"]
+crossovers = ["onepoint"]
+population_sizes = [50]
+elitisms = [2]
+mutations = [0.2]
+evolution_rounds = [50]
+backends = ["processes","threads"]
+stopping_thresholds = [0.99]
+
+if __name__ == "__main__":
+    for back in backends:
+        for popsize in population_sizes:
+            for model in models:
+                for data in datasets:
+                    for algo in algorithms:
+                        for metric in metrics:
+                            for cross in crossovers:
+                                for mut in mutations:
+                                    for elit in elitisms:
+                                        for evos in evolution_rounds:
+                                            for stopping_threshold in stopping_thresholds:
+
+                                                file = f"--population_size={popsize} "\
+                                                f"--evolution_rounds={evos} "\
+                                                f"--crossover_choice={cross} "\
+                                                f"--metric_choice={metric} "\
+                                                f"--elitism={elit} "\
+                                                f"--dataset={data} "\
+                                                f"--algorithm={algo} "\
+                                                f"--model={model} "\
+                                                f"--backend_prefer={back} "\
+                                                f"--mutation_rate={mut} "\
+                                                f"--stopping_threshold={stopping_threshold}"
+
+                                                # print(file)
+                                                if blocking:
+                                                    file_full = f"python ga_fs.py {file}"
+                                                    print(f'Running: {file_full}')
+                                                    os.system(file_full)
+                                                else:
+                                                    file_full = f"python ga_fs.py {file}"
+                                                    print(f"Running: sbatch execute.bash '{file_full}'")
+                                                    Popen(f"sbatch execute.bash '{file_full}'",shell=True)
+
+                                                time.sleep(0.01)
---
+++ execute.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+#set a job name
+#SBATCH --job-name=ga
+#a file for job output, you can check job progress
+#SBATCH --output=logs/run_%j.out
+# a file for errors from the job
+#SBATCH --error=logs/run_%j.err
+#time you think you need: default is one day
+#in minutes in this case, hh:mm:ss
+#SBATCH --time=24:00:00
+#number of cores you are requesting
+#SBATCH --cpus-per-task=50
+#memory you are requesting
+#SBATCH --mem=100Gb
+#partition to use
+#SBATCH --partition=short
+
+module load anaconda3/2022.05
+source activate /home/yildiz.ay/.conda/envs/ga
+
+srun $1
