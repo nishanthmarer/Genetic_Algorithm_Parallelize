3aa9ec62446a45c83faf202af81c28822c554767
nishanthmarer <125854808+nishanthmarer@users.noreply.github.com>
2023-12-10 15:51:23 -0500

    Add files via upload

--- discovery_submit.py
+++ discovery_submit.py
@@ -6,24 +6,43 @@ from subprocess import Popen
 import pdb
 
 import time
+import numpy as np
+import random
+
+random.seed(123)
+np.random.seed(123)
 
 os.makedirs(os.path.join("logs"),exist_ok=True)
 
 blocking=False
+test_run=False
 
+if test_run:
+    models = ["mlp"]
+    datasets = ["gina_agnostic"]
+    algorithms = ["ga_spark"]
+    metrics = ["f1"]
+    crossovers = ["onepoint"]
+    population_sizes = [10]
+    elitisms = [2]
+    mutations = [0.2]
+    evolution_rounds = [10]
+    backends = ["processes"]
+    stopping_thresholds = [0.99999]
 
-
-models = ["logistic","mlp","xgboost"]
-datasets = ["gina_agnostic","hiva_agnostic","sylva_agnostic"]
-algorithms = ["ga_seq","ga_joblib","random","rfs"]
-metrics = ["f1"]
-crossovers = ["onepoint"]
-population_sizes = [50]
-elitisms = [2]
-mutations = [0.2]
-evolution_rounds = [50]
-backends = ["processes","threads"]
-stopping_thresholds = [0.99]
+else:
+    models = ["logistic","mlp","xgboost"]
+    datasets = ["gina_agnostic","hiva_agnostic","sylva_agnostic"]
+    algorithms = ["baseline_metrics","ga_seq","ga_joblib","random","rfs","ga_spark"]
+    metrics = ["f1"]
+    crossovers = ["onepoint"]
+    population_sizes = [150]
+    elitisms = [2]
+    mutations = [0.2]
+    evolution_rounds = [150]
+    backends = ["threads"]
+    # backends = ["processes"]
+    stopping_thresholds = [0.99999]
 
 if __name__ == "__main__":
     for back in backends:
@@ -37,7 +56,8 @@ if __name__ == "__main__":
                                     for elit in elitisms:
                                         for evos in evolution_rounds:
                                             for stopping_threshold in stopping_thresholds:
-
+                                                if algo != "ga_joblib" and back == "threads":
+                                                    continue
                                                 file = f"--population_size={popsize} "\
                                                 f"--evolution_rounds={evos} "\
                                                 f"--crossover_choice={cross} "\
